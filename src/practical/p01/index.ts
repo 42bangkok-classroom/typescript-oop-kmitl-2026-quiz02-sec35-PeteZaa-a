858c654444ace9513e11596393d543f0adede8bf
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-01-16 11:00:54 +0700

    commit

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -3,39 +3,40 @@
 import axios from "axios";
 
 interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+  userId: number,
+  id: number,
+  title: string,
+  body: string
 }
 
 interface EdgePost {
-  id: number;
-  title: string;
+  id: number,
+  title: string
 }
 
 async function getEdgePosts(): Promise<EdgePost[]> {
-  const url = "https://jsonplaceholder.typicode.com/posts";
+  const url = "https://jsonplaceholder.typicode.com/posts"
 
   try {
-    const response = await axios.get<Post[]>(url);
-    const posts = response.data;
+    const response = await axios.get<Post[]>(url)
+    const posts = response.data
 
     if (posts.length === 0) {
-      return [];
+      return []
     }
 
-    const firstItem = posts[0];
-    const lastItem = posts[posts.length - 1];
+    const first = posts[0]
+    const last = posts[posts.length - 1]
 
-    const edgePosts: EdgePost[] = [firstItem, lastItem].map(post => ({
+    const edgePosts: EdgePost[] = [first, last].map(post => ({
       id: post.id,
       title: post.title
-    }));
+    }))
 
-    return edgePosts;
-
+    return edgePosts
+    
+    
   } catch (error) {
-    throw new Error("Failed to fetch posts");
+    throw new Error("Failed to fetch posts")
   }
 }
